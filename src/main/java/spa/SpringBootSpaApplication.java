ff022cb4b189cfa896646787e70d3ceb7ff2b546
Cliff W. Lee <changho.lee@gmail.com>
2016-03-04 12:55:12 +0900

    Initial Commit

---
+++ src/main/java/spa/SpringBootSpaApplication.java
@@ -0,0 +1,12 @@
+package spa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpaApplication.class, args);
+	}
+}
---
+++ src/test/java/spa/SpringBootSpaApplicationTests.java
@@ -0,0 +1,18 @@
+package spa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringBootSpaApplication.class)
+@WebAppConfiguration
+public class SpringBootSpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
